ed983f7adf47dace483bf3a9ea4835cf14e7fc38
mstuff <mstuff@posteo.de>
2022-06-02 17:05:43 +0200

    Backend: refactor service and controller tests

--- backend/src/test/java/com/github/mstuff/backend/controller/ScheduleEntryControllerTest.java
+++ backend/src/test/java/com/github/mstuff/backend/controller/ScheduleEntryControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.Instant;
@@ -40,9 +39,16 @@ class ScheduleEntryControllerTest {
                 .title("Appointment1")
                 .description("description1")
                 .entryDate(Instant.parse("2022-05-28T22:00:00.000Z"))
-                .entryTime(Instant.parse("2022-05-28T22:10:00.000Z"))
+                .durationInMinutes(400)
                 .build();
-        scheduleEntryRepository.insert(scheduleEntry1);
+        ScheduleEntry scheduleEntry2 = ScheduleEntry.builder()
+                .id("456")
+                .title("Appointment2")
+                .description("description3")
+                .entryDate(Instant.parse("2022-06-28T22:00:00.000Z"))
+                .durationInMinutes(600)
+                .build();
+        scheduleEntryRepository.insert(scheduleEntry1, scheduleEntry2);
 
         //WHEN
         List<ScheduleEntry> actual = webTestClient.get()
@@ -59,8 +65,15 @@ class ScheduleEntryControllerTest {
                 .title("Appointment1")
                 .description("description1")
                 .entryDate(Instant.parse("2022-05-28T22:00:00.000Z"))
-                .entryTime(Instant.parse("2022-05-28T22:10:00.000Z"))
-                .build());
+                .durationInMinutes(400)
+                .build(),
+                ScheduleEntry.builder()
+                        .id("456")
+                        .title("Appointment2")
+                        .description("description3")
+                        .entryDate(Instant.parse("2022-06-28T22:00:00.000Z"))
+                        .durationInMinutes(600)
+                        .build());
 
         assertEquals(expected, actual);
     }
@@ -73,7 +86,7 @@ class ScheduleEntryControllerTest {
                 .title("Appointment1")
                 .description("description1")
                 .entryDate(Instant.parse("2022-05-28T22:00:00.000Z"))
-                .entryTime(Instant.parse("2022-05-28T22:10:00.000Z"))
+                .durationInMinutes(400)
                 .build();
 
         //WHEN
@@ -92,7 +105,7 @@ class ScheduleEntryControllerTest {
         assertEquals("Appointment1", actual.getTitle());
         assertEquals("description1", actual.getDescription());
         assertEquals(Instant.parse("2022-05-28T22:00:00.000Z"), actual.getEntryDate());
-        assertEquals(Instant.parse("2022-05-28T22:10:00.000Z"), actual.getEntryTime());
+        assertEquals(400, actual.getEntryTime());
         assertEquals(24, actual.getId().length());
     }
 
@@ -103,7 +116,7 @@ class ScheduleEntryControllerTest {
         DtoNewScheduleEntry dtoNewEntry = DtoNewScheduleEntry.builder()
                 .description("description1")
                 .entryDate(Instant.parse("2022-05-28T22:00:00.000Z"))
-                .entryTime(Instant.parse("2022-05-28T22:10:00.000Z"))
+                .durationInMinutes(400)
                 .build();
 
         //WHEN //THEN
@@ -124,7 +137,7 @@ class ScheduleEntryControllerTest {
                 .title("Title1")
 
                 .entryDate(Instant.parse("2022-05-28T22:00:00.000Z"))
-                .entryTime(Instant.parse("2022-05-28T22:10:00.000Z"))
+                .durationInMinutes(400)
                 .build();
 
         //WHEN //THEN
@@ -144,7 +157,7 @@ class ScheduleEntryControllerTest {
                 .title("Title1")
                 .description("Description")
 
-                .entryTime(Instant.parse("2022-05-28T22:10:00.000Z"))
+                .durationInMinutes(400)
                 .build();
 
         //WHEN //THEN
