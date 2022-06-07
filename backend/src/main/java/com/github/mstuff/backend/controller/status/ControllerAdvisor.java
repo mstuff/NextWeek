ac423f3cec3b3b19bfb6306fd6709d7409d9e7eb
mstuff <mstuff@posteo.de>
2022-06-07 23:07:46 +0200

    Backend: add delete endpoint

--- backend/src/main/java/com/github/mstuff/backend/controller/status/ControllerAdvisor.java
+++ backend/src/main/java/com/github/mstuff/backend/controller/status/ControllerAdvisor.java
@@ -10,6 +10,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @ControllerAdvice
@@ -19,12 +20,26 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleConflict(IllegalArgumentException ex, WebRequest request){
 
         ErrorResponse bodyOfResponse = ErrorResponse.builder()
-                .errorMessage("There was an illegal argument in the request." + ex.getMessage())
+                .errorMessage("There was an illegal argument in the request: " + ex.getMessage())
                 .timeStamp(LocalDateTime.now())
                 .requestUri(request.getDescription(false))
                 .build();
 
-        log.error("There was an illegal argument in the request", ex);
+        log.error("There was an illegal argument in the request ", ex);
+
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = {NoSuchElementException.class})
+    protected ResponseEntity<Object> handleConflict(NoSuchElementException ex, WebRequest request){
+
+        ErrorResponse bodyOfResponse = ErrorResponse.builder()
+                .errorMessage("This element does not exist: " + ex.getMessage())
+                .timeStamp(LocalDateTime.now())
+                .requestUri(request.getDescription(false))
+                .build();
+
+        log.error("This element does not exist. ", ex);
 
         return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
